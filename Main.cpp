31a8c71e818b48a75dacfe932297c99dc21213c8
Modak, Bikram <bikram.modak@intel.com>
2022-11-07 22:10:49 +0530

    Shader code enhancement

--- Main.cpp
+++ Main.cpp
@@ -9,13 +9,13 @@
 
 // Vertices coordinates
 GLfloat vertices[] =
-{
-    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-    0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-    0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-    -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-    0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-    0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+{ //               COORDINATES                  /     COLORS           //
+    -0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+     0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+     0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+    -0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+     0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+     0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
 };
 
 // Indices for vertices order
@@ -32,8 +32,7 @@ void error_callback(int error, const char* msg) {
     std::cerr << s << std::endl;
 }
 
-
-int main()
+void setupGl()
 {
     glfwSetErrorCallback(error_callback);
     // Initialize GLFW
@@ -48,17 +47,23 @@ int main()
     // Tell GLFW we are using the CORE profile
     // So that means we only have the modern functions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
 
 
-    // Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-    GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+int main()
+{
+    setupGl();
+    // Create a GLFWwindow object of 800 by 800 pixels, naming it "LearningOpenGL"
+    GLFWwindow* window = glfwCreateWindow(800, 800, "LearningOpenGL", NULL, NULL);
     // Error check if the window fails to create
     if (window == NULL)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        error_callback(-1, "Failed to create GLFW window");
         glfwTerminate();
         return -1;
     }
+
+
     // Introduce the window into the current context
     glfwMakeContextCurrent(window);
 
@@ -84,13 +89,16 @@ int main()
     // Generates Element Buffer Object and links it to indices
     EBO EBO1(indices, sizeof(indices));
 
-    // Links VBO to VAO
-    VAO1.LinkVBO(VBO1, 0);
+    // Links VBO attributes such as coordinates and colors to VAO
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     // Unbind all to prevent accidentally modifying them
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
 
+    // Gets ID of uniform called "scale"
+    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 
     // Main while loop
@@ -102,6 +110,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         // Tell OpenGL which Shader Program we want to use
         shaderProgram.Activate();
+        // Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
+        glUniform1f(uniID, 0.5f);
         // Bind the VAO so OpenGL knows to use it
         VAO1.Bind();
         // Draw primitives, number of indices, datatype of indices, index of indices
